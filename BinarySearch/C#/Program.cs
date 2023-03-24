992ded646512dc07c05ae199982a5052d1281ad1
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-23 23:52:08 -0600

    Updated .gitignore

---
+++ BinarySearch/C#/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> list = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int varToFind = 3;
+Console.WriteLine(varToFind); 
+
+
