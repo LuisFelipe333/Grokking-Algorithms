670eafb7f1bf9b34ed570ff447e85e68d1b78f88
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-24 00:07:38 -0600

    Added binary search in C#

--- BinarySearch/C#/BinarySearch.cs
+++ BinarySearch/C#/BinarySearch.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace C_
 {
-    internal class BinarySearch
+    public class BinarySearch
     {
+        
+
+        public static void Main(string[] args)
+        {
+            List<dynamic> list = new List<dynamic>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int varToFind = 12;
+            int finded = binarySearch(list, varToFind);
+            Console.WriteLine(finded);
+        }
+        
+        public static dynamic binarySearch(List<dynamic> list, dynamic item)
+        {
+            int low = 0;
+            int high = list.Count - 1;
+            int mid;
+            dynamic guess;
+
+            while (low <= high)
+            {
+                mid = low + high;
+                guess = list[mid];
+                if (guess == item)
+                    return mid;
+                if (guess > item)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            return -1;
+
+        }
+
     }
 }
